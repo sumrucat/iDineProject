5e3d0c579df8d17224ecc83c86d85fc4af679db4
Suman Singh Bhati <suman.bhati@itgurussoftware.com>
2022-06-03 12:31:35 +0530

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Apple on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
